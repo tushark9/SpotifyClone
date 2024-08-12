a04bf868a191c9a9c80deab0a3104d8ccca69c50
Tushar <tusharkathuria41051@gmail.com>
2024-08-12 20:26:44 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import PlayerContextProvider from './context/PlayerContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/spotify-clone">
+    <BrowserRouter basename="/SpotifyClone">
       <PlayerContextProvider>
         <App />
       </PlayerContextProvider>
